133e20c727c22ba4a22f3c73fd35a049e85b7a6a
stalertelie <stalertelie@gmail.com>
2017-11-07 20:30:26 +0000

    add layoutfile and test for title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,10 +4,17 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+# la page d'accueil doit etre ruby on rails ... note li 8.
+    assert_select "title","Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 
