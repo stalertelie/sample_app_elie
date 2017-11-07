d4b3a43d23e6d0b3bb17de5d9f2088306a2390eb
stalertelie <stalertelie@gmail.com>
2017-11-07 20:01:28 +0000

    add new element at view

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   get 'static_pages/help'
-
-  get 'sample_app_elie/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'application#hello'
+  
+  root 'application#hello'
 end
